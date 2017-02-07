f522ea044816123badccac8cb772f7a1507bcd53
Fernando Ruaro <landell.ruaro@gmail.com>
2017-02-07 09:37:24 -0200

    Initial commit

---
+++ terraform/cluster.tf
@@ -0,0 +1,7 @@
+variable "region" { default="us-west-2" }
+
+
+resource "aws_key_pair" "deployer" {
+  key_name = "kubernetes_tf" 
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCfCxovRTyz8cGnhj8tgUV7gK+u7CCOKXgICX9BVPo5EAHAP8WSmCofh8RnFTsajUkJA6NBKElzNNe9UpU8mgC9XZ9UQ2viG3KmLwXPxnONKipCGp0mUGWSp4p9uVi97nc4dTmBe7bTGRLoozBGi24Pm/80kDLAxlMnNk+j4jNjEGIvPG58Jc1W0qMqegRLfYup4ZGVOWHkHGPfz9/K3f5fNSDscVur1FLSKHq8pu9n0N43J+p8rpVKYZZt5By1JsJq1+mfdhcrGQyho2ejIyqyr1lS06NMJ9wcVYi5mRldfyNq/oMDYc/utXeLx8hredeA7gRZrWpzlS0cbY/F4ran ec2-user@ip-172-31-26-212"
+}
