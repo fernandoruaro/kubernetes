7a8504a46a5cb53e0cc85b3598ecb9526906654c
Fernando Ruaro <landell.ruaro@gmail.com>
2017-02-07 10:26:47 -0200

    Adding first etcd cluster

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -1,7 +1,33 @@
 variable "region" { default="us-west-2" }
 provider "aws" { region = "${var.region}" }
 
-resource "aws_key_pair" "deployer" {
+resource "aws_key_pair" "kubernetes" {
   key_name = "kubernetes_tf" 
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCfCxovRTyz8cGnhj8tgUV7gK+u7CCOKXgICX9BVPo5EAHAP8WSmCofh8RnFTsajUkJA6NBKElzNNe9UpU8mgC9XZ9UQ2viG3KmLwXPxnONKipCGp0mUGWSp4p9uVi97nc4dTmBe7bTGRLoozBGi24Pm/80kDLAxlMnNk+j4jNjEGIvPG58Jc1W0qMqegRLfYup4ZGVOWHkHGPfz9/K3f5fNSDscVur1FLSKHq8pu9n0N43J+p8rpVKYZZt5By1JsJq1+mfdhcrGQyho2ejIyqyr1lS06NMJ9wcVYi5mRldfyNq/oMDYc/utXeLx8hredeA7gRZrWpzlS0cbY/F4ran ec2-user@ip-172-31-26-212"
+}
+
+
+resource "aws_vpc" "kubernetes" {
+  cidr_block = "10.43.0.0/16"
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "kubernetes" {
+  vpc_id = "${aws_vpc.kubernetes.id}"
+  cidr_block = "10.43.0.0/16"
+  availability_zone = "eu-west-1a"
+}
+
+resource "aws_instance" "etcd" {
+    count = 3
+    ami = "ami-1967056a" // Unbuntu 16.04 LTS HVM, EBS-SSD
+    instance_type = "t2.micro"
+
+    subnet_id = "${aws_subnet.kubernetes.id}"
+    private_ip = "${cidrhost("10.43.0.0/16", 10 + count.index)}"
+    associate_public_ip_address = true
+
+    availability_zone = "eu-west-1a"
+    vpc_security_group_ids = ["${aws_security_group.kubernetes.id}"]
+    key_name = "#{aws_key_pair.kubernetes.key_name}"
 }
