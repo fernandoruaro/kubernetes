d9d59523f12963870d3eac7f2715d1b45bb9103b
Fernando Ruaro <landell.ruaro@gmail.com>
2017-02-08 07:13:13 -0200

    Adding external access

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -130,3 +130,21 @@ resource "aws_instance" "etcd_extra" {
         kubernetes_role = "etcd"
     }
 }
+
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.kubernetes.id}"
+}
+
+resource "aws_route_table" "kubernetes" {
+    vpc_id = "${aws_vpc.kubernetes.id}"
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = "${aws_internet_gateway.gw.id}"
+    }
+}
+
+resource "aws_route_table_association" "kubernetes" {
+  subnet_id = "${aws_subnet.kubernetes.id}"
+  route_table_id = "${aws_route_table.kubernetes.id}"
+}
