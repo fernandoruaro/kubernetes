ba2e685b50230f0486279ca99c4a206f53193eea
Fernando Ruaro <landell.ruaro@gmail.com>
2017-02-26 14:08:23 -0800

    Organizing new relic variables

--- pass_var_terraform_to_ansible.sh
+++ pass_var_terraform_to_ansible.sh
@@ -10,6 +10,7 @@ JSON=$JSON"\"etcd_key_id\":\"$(echo -n $(terraform output etcd_key_id) | base64 
 JSON=$JSON"\"etcd_key_secret\":\"$(echo -n $(terraform output etcd_key_secret) | base64 -w 0)\","
 JSON=$JSON"\"aws_region\":\"$(terraform output aws_region)\","
 JSON=$JSON"\"s3_etcd_backup_bucket\":\"$(terraform output s3_etcd_backup_bucket)\","
+JSON=$JSON"\"cluster_name\":\"$(terraform output cluster_name)\","
 JSON=$JSON"\"newrelic_labels\":\"kubernetes_cluster:$(terraform output cluster_name)\","
 JSON=$JSON"\"kubernetes_etcd_url\":\"$(terraform output kubernetes_etcd_url)\""
 JSON=$JSON"}"
