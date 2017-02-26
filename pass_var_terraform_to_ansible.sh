22e1f3713f3f6919c510f4df6fabd77606b1104e
Fernando Ruaro <landell.ruaro@gmail.com>
2017-02-25 18:51:01 -0800

    Secret config

--- pass_var_terraform_to_ansible.sh
+++ pass_var_terraform_to_ansible.sh
@@ -6,6 +6,8 @@ touch $VAR_FILE
 JSON="{"
 JSON=$JSON"\"kubernetes_route_table_id\":\"$(terraform output kubernetes_route_table_id)\","
 JSON=$JSON"\"kubernetes_master_url\":\"$(terraform output kubernetes_master_url)\","
+JSON=$JSON"\"etcd_key_id\":\"$(terraform output etcd_key_id | base64 -w 0)\","
+JSON=$JSON"\"etcd_key_secret\":\"$(terraform output etcd_key_secret | base64 -w 0)\","
 JSON=$JSON"\"aws_region\":\"$(terraform output aws_region)\","
 JSON=$JSON"\"kubernetes_etcd_url\":\"$(terraform output kubernetes_etcd_url)\""
 JSON=$JSON"}"
