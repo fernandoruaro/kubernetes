0ce38732906c9a253d2e911164bfb83399ac936b
Fernando Ruaro <landell.ruaro@gmail.com>
2017-02-26 11:48:18 -0800

    Adding etcd cron

--- pass_var_terraform_to_ansible.sh
+++ pass_var_terraform_to_ansible.sh
@@ -9,6 +9,7 @@ JSON=$JSON"\"kubernetes_master_url\":\"$(terraform output kubernetes_master_url)
 JSON=$JSON"\"etcd_key_id\":\"$(echo -n $(terraform output etcd_key_id) | base64 -w 0)\","
 JSON=$JSON"\"etcd_key_secret\":\"$(echo -n $(terraform output etcd_key_secret) | base64 -w 0)\","
 JSON=$JSON"\"aws_region\":\"$(terraform output aws_region)\","
+JSON=$JSON"\"s3_etcd_backup_bucket\":\"$(terraform output s3_etcd_backup_bucket)\","
 JSON=$JSON"\"kubernetes_etcd_url\":\"$(terraform output kubernetes_etcd_url)\""
 JSON=$JSON"}"
 echo $JSON > $VAR_FILE
