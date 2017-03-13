248e606cf9fcb6e9fd9a76f162387fc4d8ab4953
Fernando Ruaro <landell.ruaro@gmail.com>
2017-03-13 19:52:29 -0300

    Outputs iam profile

--- terraform/output.tf
+++ terraform/output.tf
@@ -41,3 +41,7 @@ output "subnet_ids" {
 output "security_group_id" {
 	value = "${aws_security_group.kubernetes.id}"
 }
+
+output "iam_instance_profile_id" {
+	value = "${aws_iam_instance_profile.kubernetes.id}"
+}
